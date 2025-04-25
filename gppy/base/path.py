3345459a6c18dd946a02d776603be9329a9960ac
7ds-snu <7dt.pipeline@gmail.com>
2025-04-25 12:17:16 +0900

    upto default paths

--- gppy/base/path.py
+++ gppy/base/path.py
@@ -1,12 +1,14 @@
 from pathlib import Path
 from .. import const
 import numpy as np
-from ..utils import parse_key_params_from_header, parse_key_params_from_filename
+from ..utils import parse_key_params_from_header#, parse_key_params_from_filename
 import re
 import os
-image_unique_keys = ["date", "gain", "filter", "obj", "unit", "n_binning", "exposure"]
+image_unique_keys = ["date", "filter", "obj", "unit", "exposure"]
 
 class PathHandler:
+
+    base_path = Path("/lyman/data2/")
 
     def __init__(self, input):
         if isinstance(input, str):
@@ -18,69 +20,76 @@ class PathHandler:
         else:
             raise TypeError("Input must be a string, Path, or list of strings/Paths.")
         
-        if not(self.is_present(self.input_file)):
-            raise FileNotFoundError(f"Path does not exist: {self.input_file}")
+        if not(self.is_present(self._input_file)):
+            raise FileNotFoundError(f"Path does not exist: {self._input_file}")
         
-        self.check_params()
+        self.define_common_path()
 
     def __getattr__(self, name):
-        if name.startswith("__"):
-            return None
-        if hasattr(self, "_"+name) :
-            return getattr(self, "_"+name)
-        else:
+        if name.endswith("_to_string"):
+            tmp_name = name.replace("_to_string", "")
+            if getattr(self, tmp_name) is not None:
+                if isinstance(getattr(self, tmp_name), Path):
+                    return str(getattr(self, tmp_name).absolute())
+                elif isinstance(getattr(self, tmp_name), str):
+                    return getattr(self, tmp_name)
+                else:
+                    return getattr(self, tmp_name)
+        elif name.endswith("_to_path"):
+            temp_name = name.replace("_to_path", "")
+            if getattr(self, temp_name) is not None:
+                if isinstance(getattr(self, temp_name), str):
+                    return Path(getattr(self, temp_name))
+                elif isinstance(getattr(self, temp_name), Path):
+                    return getattr(self, temp_name)
+                else:
+                    return getattr(self, tmp_name)
+        elif name in self.__dict__:
             return getattr(self, name)
+        else:
+            raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{name}'")
+
+    def __repr__(self):
+        return "\n".join(f"{k}: {v}" for k, v in self.__dict__.items() if not k.startswith("_"))
     
     def is_present(self, path):
         paths = np.atleast_1d(path)
         return all([p.exists() for p in paths])
         
     def define_common_path(self):
-        if const.RAWDATA_DIR in str(self.input_file[0]):
+        if const.RAWDATA_DIR in str(self._input_file[0]):
+            params = check_params(self._input_file[0])
             self.data_type = "raw"
-            self._raw_image= [str(file.absolute()) for file in self.input_file]
-            _tmp_relpath = os.path.join(self.date, self.obj, self.filter, self.unit)
-            self._output_dir = Path(os.path.join(const.PROCESSED_DIR, _tmp_relpath))
-            self._factory_dir = Path(os.path.join(const.FACTORY_DIR, _tmp_relpath))
-            self._file_prefix = [
-                switch_name_order(file.stem) for file in self.input_file
+            self.raw_image= [str(file.absolute()) for file in self._input_file]
+            _tmp_relpath = os.path.join(params["date"], params["obj"], params["filter"], params["unit"])
+            self.output_dir = os.path.join(const.PROCESSED_DIR, _tmp_relpath)
+            self.factory_dir = os.path.join(const.FACTORY_DIR, _tmp_relpath)
+            self.masterframe_dir = os.path.join(const.FACTORY_DIR, _tmp_relpath)
+            self.file_prefix = [
+                switch_name_order(file.stem) for file in self._input_file
             ]
-            self._processed_image = [
-                os.path.join(self.output_dir, "images", file_prefix, ".fits") for file_prefix in self._file_prefix
-            ]            
-        elif const.PROCESSED_DIR in str(self.input_file[0]):
+            self.processed_image = [
+                os.path.join(self.output_dir, "images", file_prefix+".fits") for file_prefix in self.file_prefix
+            ]
+        elif const.PROCESSED_DIR in str(self._input_file[0]):
             self.data_type = "processed"
-            self._processed_image = [str(file.absolute()) for file in self.input_file]
-            self._file_prefix = [file.stem for file in self.input_file]
-            
-            #self._factory_dir = Path(os.path.join(const.FACTORY_DIR, self.date, self.obj, self.filter, self.unit)
+            self.processed_image = [str(file.absolute()) for file in self._input_file]
+            self.file_prefix = [file.stem for file in self._input_file]
+            self.factory_dir = os.path.join(const.FACTORY_DIR, *Path(self._input_file[0]).parts[-6:-3])
+            self.output_dir = self._input_file[0].parent.parent
         else:
             self.data_type = "user-input"
-            
-    
-    def define_common_path(self):
-        self._output_dir = self._processed_image[0].parent.parent
-
-    def check_params(self):
-        try:
-            params = [parse_key_params_from_filename(img)[0] for img in self.input_file]
-        except:
-            try:
-                params = [parse_key_params_from_header(img)[0] for img in self.input_file]
-            except:
-                raise ValueError("No parameters found in the image file names or headers.")
-        if not params:
-            raise ValueError("No parameters found in the image file names or headers.")
-        def all_dicts_equal(lst):
-            return all(d == lst[0] for d in lst)
-        if not all_dicts_equal(params):
-            raise ValueError("Not all images have the same parameters.")
-        else:
-            for key in image_unique_keys:
-                setattr(self, "_"+key, params[0][key])
-
-    def path_configuration(self):
-        pass
+            print("User input data type detected. Assume the input is a list of processed images.")
+            self.processed_image = [str(file.absolute()) for file in self._input_file]
+            self.file_prefix = [file.stem for file in self._input_file]
+            self.output_dir = self._input_file[0].parent.parent
+            self.factory_dir = self._input_file[0].parent.parent / "factory"
+        
+        self.image_dir = os.path.join(self.output_dir, "images")
+        self.figure_dir = os.path.join(self.output_dir, "figures")
+        self.daily_stacked_dir = os.path.join(self.output_dir, "stacked")
+        self.subtracted_dir = os.path.join(self.output_dir, "subtracted")
+        self.ref_sex_dir = Path(os.path.join(const.REF_DIR, "srcExt"))
   
     def path_preprocess(self):
         pass
@@ -93,46 +102,6 @@ class PathHandler:
 
     def path_stacking(self):
         pass
-
-
-
-#     def define_paths(self):
-#         """Create and set output directory paths for processed data."""
-#         _date_dir = define_output_dir(self.config.obs.date, self.config.obs.n_binning, self.config.obs.gain)
-
-#         rel_path = os.path.join(
-#             _date_dir,
-#             self.config.obs.object,
-#             self.config.obs.unit,
-#             self.config.obs.filter,
-#         )
-#         fdz_rel_path = os.path.join(
-#             _date_dir,
-#             self.config.obs.unit,
-#         )
-
-#         path_processed = os.path.join(self._output_prefix, rel_path)
-#         path_factory = os.path.join(FACTORY_DIR, rel_path)
-#         path_fdz = os.path.join(MASTER_FRAME_DIR, fdz_rel_path)
-#         path_stacked_prefix = STACKED_DIR if self.config.name == "user-input" else DAILY_STACKED_DIR
-#         path_stacked = os.path.join(path_stacked_prefix, rel_path)
-
-#         os.makedirs(path_processed, exist_ok=True)
-#         os.makedirs(path_fdz, exist_ok=True)
-#         os.makedirs(path_factory, exist_ok=True)
-#         # os.makedirs(path_stacked, exist_ok=True)  # make dir in imstack
-
-#         self.config.path.path_processed = path_processed
-#         self.config.path.path_stacked = path_stacked
-#         self.config.path.path_factory = path_factory
-#         self.config.path.path_fdz = path_fdz
-#         self.config.path.path_raw = find_raw_path(
-#             self.config.obs.unit,
-#             self.config.obs.date,
-#             self.config.obs.n_binning,
-#             self.config.obs.gain,
-#         )
-#         self.config.path.path_sex = os.path.join(REF_DIR, "srcExt")
 
 def switch_name_order(name):
     """
@@ -147,3 +116,56 @@ def switch_name_order(name):
     """
     parts = name.split("_")
     return "_".join(parts[3:5] + parts[0:1] + parts[6:7] + parts[1:3])
+
+
+def check_params(img):
+    try:
+        params = parse_key_params_from_filename(img)[0]
+    except:
+        try:
+            params = parse_key_params_from_header(img)[0]
+        except:
+            raise ValueError("No parameters found in the image file names or headers.")
+    if not params:
+        raise ValueError("No parameters found in the image file names or headers.")
+    return params
+
+def parse_key_params_from_filename(img):
+    if isinstance(img, str):
+        path = Path(img)
+    elif isinstance(img, Path):
+        path = img
+    else:
+        raise TypeError("Input must be a string or Path.")
+    
+    pattern = r"^(7DT\d{2})_(\d{8}_\d{6})_([a-zA-Z0-9]+)_([a-zA-Z0-9]+)_(\dx\d)_(\d+\.?\d*)s_([0-9]+)"
+    match = re.match(pattern, path.stem)
+        
+    if match:
+        units, date_string, target, filt, n_binning, exposure, _ = match.groups()
+        date = subtract_half_day(date_string)
+    else:
+        pattern = r"([a-zA-Z0-9]+)_([a-zA-Z0-9]+)_(7DT\d{2})_(\d+\.?\d*)s_(\d{8}_\d{6})"
+        match = re.match(pattern, path.stem)
+        if match:
+            target, filt, units, exposure, date_string = match.groups()
+            date = subtract_half_day(date_string)
+
+    info = {
+        "date": date,
+        "filter": filt,
+        "obj": target,
+        "unit": units,
+        "exposure": exposure
+    }
+
+    file_type = "master_image" if any(s in str(path.stem) for s in ["BIAS", "DARK", "FLAT"]) else "sci_image"
+
+    return info, file_type
+
+from datetime import datetime, timedelta
+
+def subtract_half_day(timestr: str) -> str:
+    dt = datetime.strptime(timestr, "%Y%m%d_%H%M%S")
+    new_dt = dt - timedelta(hours=12)
+    return new_dt.strftime("%Y-%m-%d")
