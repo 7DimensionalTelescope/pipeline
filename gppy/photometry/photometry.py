ecf9877310eefefabb2c6ae678e933a88200cded
7DT pipeline <7dt.pipeline@gmail.com>
2025-07-05 18:39:45 +0900

    fix imstack array to numpy

--- gppy/photometry/photometry.py
+++ gppy/photometry/photometry.py
@@ -315,7 +315,7 @@ class PhotometrySingle:
             self.write_catalog(obs_src_table)
 
         else:
-            self.logger.into("Skipping filter check")
+            self.logger.info("Skipping filter check")
             obs_src_table = self.add_matched_reference_catalog(obs_src_table)
             zp_dict, aper_dict, cols = self.calculate_zp(obs_src_table)
             self.add_reference_columns(obs_src_table, cols)
