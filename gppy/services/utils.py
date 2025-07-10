7d248dbf7e2257da25ccf83c1f9a5cdee3be7180
Donggeun Tak <takdg123@gmail.com>
2025-07-10 11:20:01 +0900

    fix minor

--- gppy/services/utils.py
+++ gppy/services/utils.py
@@ -364,19 +364,33 @@ def acquire_available_gpu(device_id=None, gpu_threshold=500, blocking=True, time
             continue  # Skip if lock file can't be opened
 
         t_start = time.time()
+
+           for gpu_id in available_gpus:
+        lock_path = f"/tmp/gpu{gpu_id}.lock"
+        try:
+            lock_file = open(lock_path, "w")
+        except Exception:
+            continue  # Skip if lock file can't be opened
+
+        t_start = time.time()
         while True:
             try:
                 flag = fcntl.LOCK_EX
                 if not blocking:
                     flag |= fcntl.LOCK_NB
+
                 fcntl.flock(lock_file, flag)
-                yield gpu_id  # Successfully locked
-                fcntl.flock(lock_file, fcntl.LOCK_UN)
-                lock_file.close()
+
+                try:
+                    yield gpu_id  # Success
+                finally:
+                    # Always unlock and close on exit, even on exception
+                    fcntl.flock(lock_file, fcntl.LOCK_UN)
+                    lock_file.close()
                 return
             except BlockingIOError:
                 if not blocking or (time.time() - t_start) > timeout:
-                    break  # Give up
+                    break
                 time.sleep(0.1)
 
         lock_file.close()
