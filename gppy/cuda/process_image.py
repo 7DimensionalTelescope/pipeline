dc61532245dc40c5b3d01760c30c1daed871f16d
7ds-snu <7dt.pipeline@gmail.com>
2025-07-11 16:32:04 +0900

    latest

--- gppy/cuda/process_image.py
+++ gppy/cuda/process_image.py
@@ -2,15 +2,14 @@ from astropy.io import fits
 import argparse
 import cupy as cp
 import numpy as np
+import gc
 
 # Reduction kernel
 reduction_kernel = cp.ElementwiseKernel(
     in_params="T x, T b, T d, T f", out_params="T z", operation="z = (x - b - d) / f", name="reduction"
 )
 
-def process_image_with_cupy(
-    obs, bias, dark, flat, output, device_id, **kwargs
-):
+def process_image_with_cupy(obs, bias, dark, flat, output, device_id):
     """median is GPU, std is CPU. Uses pinned memory for better host-GPU transfer performance."""
 
     with cp.cuda.Device(device_id):
@@ -31,23 +30,26 @@ def process_image_with_cupy(
         del cbias, cdark, cflat, cdata
         cp.get_default_memory_pool().free_all_blocks()
 
-    for o, d in zip(output, data):
-        header = o.replace(".fits", ".header")
-        fits.writeto(o, d, header=header, overwrite=True)
-
+    for i, o in enumerate(output):
+        header_file = o.replace(".fits", ".header")
+        with open(header_file, 'r') as f:
+            head = fits.Header.fromstring(f.read(), sep='\n')
+        fits.writeto(o, data[i], header=head, overwrite=True)
+    del data
+    gc.collect()
     return 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Process FITS images with optional subtraction, normalization, and BPMask creation using CUDA."
+        description="Process FITS images."
     )
 
+    parser.add_argument("-bias", type=str, required=True, help="BIAS FITS image path.")
+    parser.add_argument("-dark", type=str, required=True, help="DARK FITS image path.")
+    parser.add_argument("-flat", type=str, required=True,  help="FLAT image path.")
     parser.add_argument("-input", nargs="+", required=True, help="Input FITS image paths.")
-    parser.add_argument("-bias", required=True, help="Output FITS for median image.")
-    parser.add_argument("-dark", required=True, help="Output FITS for stddev image.")
-    parser.add_argument("-flat", nargs="*", default=[], help="Subtract image paths.")
-    parser.add_argument("-output", nargs="*", type=float, default=[], help="Scale factors for subtract images.")
-    parser.add_argument("-device", action="store_true", help="Apply per-image normalization.")
+    parser.add_argument("-output", nargs="+", required=True, help="Output FITS image paths.")
+    parser.add_argument("-device", type=int, default=0, help="CUDA device ID.")
 
     args = parser.parse_args()
 
@@ -57,5 +59,5 @@ if __name__ == "__main__":
         args.dark, 
         args.flat, 
         args.output,
-        device_id=args.device,
+        args.device,
     )
