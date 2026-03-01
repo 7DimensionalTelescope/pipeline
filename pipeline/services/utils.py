89ca985eb149d604357e2f1cee307b6ff4a05cd0
7ds-snu <7dt.pipeline@gmail.com>
2026-03-01 19:16:46 +0900

    fix

--- pipeline/services/utils.py
+++ pipeline/services/utils.py
@@ -700,7 +700,7 @@ class PreprocessGroup:
     def add_sci_keys(self, keys: str):
         self.sci_keys.append(keys)
 
-    def create_config(self, overwrite: bool = False, is_too: bool = False, is_pipeline: bool = False, **kwargs):
+    def create_config(self, overwrite: bool = False, is_too: bool = False, is_pipeline: bool = True, **kwargs):
         from ..config import PreprocConfiguration
 
         # print(
